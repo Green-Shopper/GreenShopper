90d035f92fcb597419c733dfe1db412d40260935
AAron Staton <Orson108@hotmail.com>
2020-03-02 16:58:59 -0500

    Created Product model fields are title, description, price, imgUrl, and stock

---
+++ server/db/models/product.js
@@ -0,0 +1,27 @@
+const Sequlize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  title: {
+    type: Sequlize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  description: {
+    type: Sequlize.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: Sequlize.FLOAT,
+    allowNull: false
+  },
+  imgUrl: {
+    type: Sequlize.STRING,
+    defaultValue: 'defaultImg.jpg'
+  },
+  stock: {
+    type: Sequlize.INTEGER
+  }
+})
+
+module.exports = Product
