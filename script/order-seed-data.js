934dad10e3ea5e3d6575ba572249120e5c3bb9ed
AAron Staton <Orson108@hotmail.com>
2020-03-03 15:00:53 -0500

    Added order-seed-data file defined orderSeedData array

---
+++ script/order-seed-data.js
@@ -0,0 +1,160 @@
+const orderSeedData = [
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: true
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  },
+  {
+    isCart: false
+  }
+]
+
+module.exports = orderSeedData
