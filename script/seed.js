28d916dec0235a117c9dd8383314abfce5bc690a
AAron Staton <Orson108@hotmail.com>
2020-03-02 21:30:37 -0500

    Added firstName and lastName entries to the seed data

--- script/seed.js
+++ script/seed.js
@@ -1,15 +1,25 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Product, Order} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({
+      email: 'cody@email.com',
+      firstName: 'cody',
+      lastName: 'bones',
+      password: '12345'
+    }),
+    User.create({
+      email: 'murphy@email.com',
+      firstName: 'murphy',
+      lastName: 'brown',
+      password: '12345'
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
