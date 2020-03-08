20e1e94731487e838135604fb369b356d7d9f2e6
AAron Staton <Orson108@hotmail.com>
2020-03-07 20:17:55 -0500

    feature -in order model, added method to merge carts

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -29,6 +29,56 @@ Order.getAllItemsInCart = async function(cartId) {
   return allCartItems
 }
 
+Order.mergeGuestCartWithUserCart = async function(
+  orderId,
+  guestCart,
+  userCart
+) {
+  let guestIndex = 0
+  const guestlength = guestCart.length
+  let userIndex = 0
+  const userlength = userCart.length
+  let combinedCart = []
+
+  const cart = await Order.findByPk(orderId)
+
+  guestCart.sort((a, b) => a.id - b.id)
+  userCart.sort((a, b) => a.id - b.id)
+
+  //add products to the combined cart in order
+  while (guestIndex < guestlength && userIndex < userlength) {
+    if (guestCart[guestIndex].id === userCart[userIndex].id) {
+      const newQty =
+        guestCart[guestIndex].quantity + userCart[userIndex].quantity
+      await Order.updateQuantity(orderId, guestCart[guestIndex].id, newQty)
+      guestIndex++
+      userIndex++
+      guestCart[guestIndex].quantity = newQty
+      combinedCart.push(guestCart[guestIndex])
+      continue
+    }
+    if (guestCart[guestIndex].id < userCart[userIndex].id) {
+      await cart.addProduct(guestCart[guestIndex])
+      combinedCart.push(guestCart[guestIndex])
+      guestIndex++
+    } else {
+      combinedCart.push(userCart[userIndex])
+      userIndex++
+    }
+  }
+  //spread remaing entries into the combinedCart array
+  if (guestIndex >= guestlength) {
+    while (guestIndex < guestlength) {
+      await cart.addProduct(guestCart[guestIndex])
+      combinedCart.push(guestCart[guestIndex])
+      guestIndex++
+    }
+  } else {
+    combinedCart = [...combinedCart, ...guestCart.slice(guestIndex)]
+  }
+  return combinedCart
+}
+
 Order.updateQuantity = async function(orderId, productId, newQty) {
   const cartItem = await OrderSummary.findOne({
     where: {
