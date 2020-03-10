b0b9d50ff4eb6f1b442a4c3b1fded5407aaf12a9
AAron Staton <Orson108@hotmail.com>
2020-03-10 15:15:13 -0400

    fix -in routes file, added a route for '/' so that it no longer goes to 404

--- client/routes.js
+++ client/routes.js
@@ -34,6 +34,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/" component={Home} />
         <Route path="/home" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
